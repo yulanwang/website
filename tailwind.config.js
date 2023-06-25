0e1b1125c46af39e41e8476cbe08595eb1f1f200
fraander <57777918+fraander@users.noreply.github.com>
2023-06-25 19:06:38 -0400

    in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        
-      }
+        'oasis-yellow': '#f0c237',
+        'oasis-yellow-pastel': '#f2d16b',
+        'oasis-green': '#397367',
+        'oasis-green-pastel': '#a7cdbd',
+        'oasis-blue': '#002642',
+        'oasis-extra-dark': '#000000',
+        'oasis-dark': '#1e2019',
+        'oasis-gray': '#6e7e85',
+        'oasis-light': '#ecf0f1',
+        'oasis-extra-light': '#FFFFFF'
+      },
     },
+  },
+  variants: {
+    fill: ['hover', 'focus'],
   },
   plugins: [],
 }
