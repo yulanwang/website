5f3c68a43fc54172d18cf2348b3069046cf6266f
fraander <57777918+fraander@users.noreply.github.com>
2023-07-05 13:27:15 -0700

    fix app/page imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hook from "@/components/Hook";
 import PageWrapper from "@/components/PageWrapper";
-import HomeAbout from "@/components/HomeAbout";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import HackSessionTimeline from "@/components/HackSessionTimeline";
 import OasisNumbers from "@/components/OasisNumbers";
