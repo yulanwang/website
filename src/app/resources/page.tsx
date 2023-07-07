4e52612b1b7af8a6506a48cf50ad410c0ee60e9d
fraander <57777918+fraander@users.noreply.github.com>
2023-07-07 09:43:31 -0700

    update redirects

--- src/app/resources/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/dist/server/api-utils";
-
-export default function Resources() {
-   redirect('https://valuable-banjo-220.notion.site/Resources-4e33d41a70ff420ba46f4e55ccb6e719?pvs=4');
-}
