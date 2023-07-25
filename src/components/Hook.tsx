9f13b397e4051c60831d7c6ab08d744a4bbd8a64
fraander <57777918+fraander@users.noreply.github.com>
2023-07-25 19:45:52 -0400

    Update Hook.tsx

--- src/components/Hook.tsx
+++ src/components/Hook.tsx
@@ -4,8 +4,8 @@ import strings from "@/app/strings";
 
 export default function Hook() {
    return (
-      <div className="bg-no-repeat overflow-hidden bg-cover landingPageImage min-h-[360px] h-[70vh] relative">
-        <div className="min-h-[360px] h-[70vh] flex flex-col justify-center items-center">
+      <div className="bg-no-repeat overflow-hidden bg-cover landingPageImage min-h-[360px] h-[80vh] relative">
+        <div className="min-h-[360px] h-[80vh] flex flex-col justify-center items-center">
           <div className="w-full min-h-[360px] max-w-[96rem] px-16 flex flex-row items-center justify-start">
             <div className="bottom-1/3 xs:left-10 xs:text-left text-center left-0 right-0 text-oasis-blue">
               <OasisTypewriter />
