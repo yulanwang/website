ac1fcf65542526de2b5a1a11f86503929227ca7c
Allen Lin <allenlinsh@gmail.com>
2023-07-27 03:19:23 +0400

    Add application short urls

---
+++ src/app/apply/students/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export default function ApplyStudentsRedirect( ) {
+  // TODO: add participants application form link
+  // redirect("paste Google Form URL here")
+}
