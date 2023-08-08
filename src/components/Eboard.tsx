24062fd76c8b8387e718d5d3e9792ee8f031b185
fraander <57777918+fraander@users.noreply.github.com>
2023-08-07 22:38:50 -0400

    add placeholder for missing image

--- src/components/Eboard.tsx
+++ src/components/Eboard.tsx
@@ -1,4 +1,6 @@
 import strings from "@/app/strings";
+import { faPerson, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 export default function Eboard() {
@@ -8,13 +10,20 @@ export default function Eboard() {
       <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-8">
         {strings.Eboard.members.map(({ name, role, src }, i) => (
           <div key={i} className="flex flex-col items-center">
-            <Image
-              className="rounded-md shadow-md mb-2"
-              src={src}
-              alt={"Profile Image for " + name}
-              width={160}
-              height={160}
-            />
+            {src !== "" ? (
+              <Image
+                className="rounded-md shadow-md mb-2"
+                src={src}
+                alt={"Profile Image for " + name}
+                width={160}
+                height={160}
+              />
+            ) : (
+              <div className="rounded-md shadow-md mb-2 w-[160px] h-[160px] bg-oa-green-pastel text-oa-light flex items-center justify-center">
+                <FontAwesomeIcon className="h-[120px]" icon={faUser} />
+              </div>
+            )}
+
             <h3>{name}</h3>
             <p>{role}</p>
           </div>
