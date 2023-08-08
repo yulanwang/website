8f59a0e18b6bf0057cc8b74bba566ef9cc86664f
fraander <57777918+fraander@users.noreply.github.com>
2023-08-07 22:54:26 -0400

    learn more metnor banner

--- src/components/BannerCard.tsx
+++ src/components/BannerCard.tsx
@@ -1,32 +1,29 @@
 import { ReactNode } from "react";
 import PrimaryButton from "./PrimaryButton";
 
+interface Button {
+  buttonTitle: string;
+  href: string;
+}
 interface Props {
   title: string;
   body: ReactNode;
-  buttonTitle: string;
-  href: string;
-  secondButtonTitle?: string; 
-  secondHref?: string; 
+  buttons: Button[];
 }
 
-export default function BannerCard({
-  title,
-  body,
-  buttonTitle,
-  href,
-  secondButtonTitle,
-  secondHref,
-}: Props) {
+export default function BannerCard({ title, body, buttons }: Props) {
   return (
     <div className="w-full flex flex-col gap-4 ring-4 my-8 mt-12 bg-oasis-yellow-pastel ring-oasis-blue rounded-xl shadow-md p-4">
       <h3 className="text-3xl text-oasis-blue">{title}</h3>
       <p className="sm:text-[20px] xs:text-[17px] xs:leading-[1.3]">{body}</p>
       <div className="flex gap-4">
-          <PrimaryButton href={href}>{buttonTitle}</PrimaryButton>
-          {secondButtonTitle && secondHref && (
-            <PrimaryButton href={secondHref}>{secondButtonTitle}</PrimaryButton>
-          )}
+        <div className="flex flex-row gap-4">
+          {buttons.map(({ buttonTitle, href }, i) => (
+            <PrimaryButton key={i} href={href}>
+              {buttonTitle}
+            </PrimaryButton>
+          ))}
+        </div>
       </div>
     </div>
   );
