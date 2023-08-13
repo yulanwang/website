2e00d5e1d789e1e260c4da094181d7f85caaa740
fraander <57777918+fraander@users.noreply.github.com>
2023-08-13 19:51:48 -0400

    Fix learn more destination

--- src/components/BannerCard.tsx
+++ src/components/BannerCard.tsx
@@ -19,7 +19,7 @@ export default function BannerCard({ title, body, buttons }: Props) {
       <div className="flex gap-4">
         <div className="flex flex-row gap-4">
           {buttons.map(({ buttonTitle, href }, i) => (
-            <PrimaryButton key={i} href={href}>
+            <PrimaryButton key={i} href={href} target="_self">
               {buttonTitle}
             </PrimaryButton>
           ))}
--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,16 +1,18 @@
-import { ReactNode } from "react";
+import { HTMLAttributeAnchorTarget, ReactNode } from "react";
 
 export default function PrimaryButton({
   href,
   children,
+  target
 }: {
-  href?: string;
+  target?: string;
+  href?: HTMLAttributeAnchorTarget;
   children: ReactNode;
 }) {
   return (
     <a
       href={href}
-      target="_blank"
+      target={target ?? "_blank"}
       className="w-fit px-4 bg-oa-extra-light hover:bg-oa-light p-2 text-lg rounded-lg ring-2 hover:ring-4 transition-all duration-200 ring-oa-blue shadow-lg hover:shadow-xl"
     >
       {children}
