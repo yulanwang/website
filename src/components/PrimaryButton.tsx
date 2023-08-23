e765c1b12df94b32ac8b9bdba828fe5df2423696
fraander <57777918+fraander@users.noreply.github.com>
2023-08-23 19:19:47 -0400

    Added not found

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,20 +1,36 @@
 import { HTMLAttributeAnchorTarget, ReactNode } from "react";
 
-interface ButtonProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>, React.AriaAttributes {
-
+interface ButtonProps
+  extends React.DetailedHTMLProps<
+      React.AnchorHTMLAttributes<HTMLAnchorElement>,
+      HTMLAnchorElement
+    >,
+    React.AriaAttributes {
+  bgColor?: string;
+  bgColorHover?: string;
+  textColor: string;
 }
 
 export default function PrimaryButton({
   children,
   href,
+  bgColor,
+  bgColorHover,
+  textColor,
   ...rest
 }: ButtonProps) {
   return (
     <a
       href={href}
       target={rest.target ?? "_blank"}
-      className="w-fit px-4 bg-oa-extra-light hover:bg-oa-light p-2 text-lg rounded-lg ring-2 hover:ring-4 transition-all duration-200 ring-oa-blue shadow-lg hover:shadow-xl"
-      {... rest}
+      className={`w-fit px-4 bg-${bgColor ?? "oa-extra-light"} hover:bg-${
+        bgColorHover ?? bgColor ?? "oa-light"
+      } p-2 text-lg text-${
+        textColor ?? "oa-blue"
+      } rounded-lg ring-2 hover:ring-4 transition-all duration-200 ring-${
+        textColor ?? "oa-blue"
+      } shadow-lg hover:shadow-xl`}
+      {...rest}
     >
       {children}
     </a>
