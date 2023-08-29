12786c14cdfbffe5644807033ac06158ccf633d3
Laith Taher <lotaher04@gmail.com>
2023-08-29 11:48:28 -0700

    page & comment fixes

--- src/app/hs-0/page.tsx
+++ src/app/hs-0/page.tsx
@@ -4,31 +4,29 @@ import Image from "next/image";
 
 /*
  * You've made it to the Hack Session 0 page! This is the page that is displayed when you visit https://oasisneu.com/hs-0
- *
- * If you're stuck, ask questions! Step by step directions can be found here -> _____
  */
 
-// Change your group's data here!
 const members = [
   {
+    // Change your data here!
     name: "Laith Taher",
     major: "Computer Science",
     graduatingYear: "2026",
+    // Add your LinkedIn profile image link here. If you do not have a LinkedIn profile then skip this step.
     image:
       "https://media.licdn.com/dms/image/D4E03AQECes9egjScKw/profile-displayphoto-shrink_800_800/0/1668552656586?e=1695859200&v=beta&t=lKPz_Un65g6Hn1ZCdtmoKV2bS86bOAj-r1ilxHjT-kQ",
   },
 ];
 
-// The only code that should be changed below this line is on line 51.
 export default function HS0() {
   return (
     <div className="bg-oa-light">
       <NavBar active="" />
       <div className="flex flex-col justify-start mt-8 mb-12">
         <h1 className="mx-auto">🌴 Hack Session 0 🌴</h1>
-        <h3 className=" mx-auto mb-12 text-4xl">Git-ting Started</h3>
+        <h3 className=" mx-auto mb-12 text-4xl">Do you Git it?</h3>
         <div className="mx-auto flex content-center justify-center pt-6">
-          <div className="grid justify-center gap-x-6 gap-y-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 xl:gap-x-8">
+          <div className="grid justify-center gap-x-6 gap-y-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 xl:gap-x-8">
             {members.map((member) => (
               <div
                 key={member.name}
