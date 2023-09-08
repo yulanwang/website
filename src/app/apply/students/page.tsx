b971dbad49ca480f0930279ddfb93c4cc558ec10
fraander <57777918+fraander@users.noreply.github.com>
2023-09-07 22:47:36 -0400

    added register link

--- src/app/apply/students/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ApplyStudentsRedirect( ) {
-  // TODO: add participants application form link
-  // redirect("paste Google Form URL here")
-}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,4 @@
+import { redirect } from "next/navigation";
+export default function F23Apply() {
+  redirect("https://forms.gle/3wKqYz9bGwtKyaNA8");
+}
