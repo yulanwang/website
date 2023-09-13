545b19503046ad674f7652331dcaaea0d4f5b016
fraander <57777918+fraander@users.noreply.github.com>
2023-09-12 21:09:03 -0400

    Commit endpoint

---
+++ src/app/commit/page.tsx
@@ -0,0 +1,4 @@
+import { redirect } from "next/navigation";
+export default function CommitRedirect() {
+  redirect("https://docs.google.com/forms/d/e/1FAIpQLSc6Y5rDnch2x-tjHSH2ZZaAfYl4hqrg1KDDBnotHx_tOsq5mw/viewform?usp=sf_link");
+}
